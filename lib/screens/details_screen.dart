8ad08477b767c2c64f41e77adf322fc996f7662c
sleatherv <sleatherv28@hotmail.com>
2022-02-23 20:25:04 -0500

    [ADD] Movie details slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-   
+
   const DetailsScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-      child: const Center(child: Text('Details Screen')),
-    )
+
+//Todo: Cambiar louego por una instancia de Movie
+final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return  const Scaffold(
+      body: CustomScrollView(
+        slivers: [ //Son widgets con cierto comportamiento pre-programado al momento del scroll del padre
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+              'movie.title',
+              style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/img/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
