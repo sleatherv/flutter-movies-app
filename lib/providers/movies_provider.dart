ad7a90b65bda9679cea4b0f1cb802377d5aad03e
sleatherv <sleatherv28@hotmail.com>
2022-02-28 19:08:35 -0500

    [ADD] http requests

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,28 @@
 
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+  String _apiKey = 'fe3645cf9bed3ff625f6344c89102ffa';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider(){
     print('MoviesProvider initialized');
-    this.getOnDiplayMovies();
+    getOnDisplayMovies();
   }
-    getOnDiplayMovies() async{
-      print('getOnDisplayMovies');
+    getOnDisplayMovies() async{
+      var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': '1',
+
+      });
+      // Await the http get response, then decode the json-formatted response.
+      final response = await http.get(url);
+      final Map<String, dynamic> decodedData = convert.jsonDecode(response.body);
+      print(decodedData['dates']);
     }
 }
