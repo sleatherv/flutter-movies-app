7a0378f57501daa266535f22b55f1e6610b41bbe
sleatherv <sleatherv28@hotmail.com>
2022-03-06 21:27:16 -0500

    Showing movies based on query

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -66,7 +66,7 @@ class MoviesProvider extends ChangeNotifier{
       return creditsResponse.cast;
     }
   
-  Future<List<Movie>> searchMovie(String query) async{
+  Future<List<Movie>> searchMovies(String query) async{
     
     final url = Uri.https(_baseUrl, '3/search/movie', {
         'api_key': _apiKey,
--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/models/models.dart';
+import 'package:peliculas_app/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate{
 
@@ -28,14 +31,54 @@ class MovieSearchDelegate extends SearchDelegate{
     return const Text('buildResults');
   }
 
+  Widget _emptyContainer(){
+    return const Center(
+        child: Icon(Icons.movie_creation_outlined, color: Colors.black38, size: 100),
+      );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     if(query.isEmpty){
-      return const Center(
-        child: Icon(Icons.movie_creation_outlined, color: Colors.black38, size: 100),
-      );
+      return _emptyContainer();
     }
-    return Container();
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+        
+        if(!snapshot.hasData) return _emptyContainer();
+        final movies = snapshot.data!;
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (_, index) => _MovieItem(movies[index]),
+        
+        );
+      },
+    );
   }
 
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/img/no-image.jpg'),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.fill,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: (){
+        Navigator.pushNamed(context, 'details', arguments: movie);
+      },
+    );
+  }
 }
